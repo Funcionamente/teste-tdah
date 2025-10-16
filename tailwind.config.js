5bc081f349e8e124ec110c938e8dfbd08095ba0f
Funcionamente <funcionamente.oficial@gmail.com>
2025-10-15 23:01:23 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        inter: ['Inter', 'sans-serif'],
-      },
-      boxShadow: {
-        soft: '0 8px 30px rgba(0, 0, 0, 0.05)',
-      },
+theme: {
+  extend: {
+    fontFamily: {
+      inter: ["var(--font-inter)"],
     },
   },
-  plugins: [],
-}
+},
