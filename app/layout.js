08eca61765eb04b8b7d261f44fb7c3150564fc19
Funcionamente <funcionamente.oficial@gmail.com>
2025-10-15 23:26:52 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body className="font-sans">{children}</body>
+      <body className="font-sans bg-gradient-to-b from-indigo-50 via-white to-pink-50">
+        {children}
+      </body>
     </html>
   );
 }
