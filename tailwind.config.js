d16b085805a929ad4251bbb594923fe364c31990
Funcionamente <funcionamente.oficial@gmail.com>
2025-10-17 23:25:48 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,17 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orangeGold: "#FFA733"
+      },
+      boxShadow: {
+        float: "0 8px 32px rgba(0,0,0,0.3)"
+      }
+    },
   },
   plugins: [],
 };
