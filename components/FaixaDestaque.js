5d2182598105bd090eb649c0c9cdc8defd59f9b1
Funcionamente <funcionamente.oficial@gmail.com>
2025-10-18 17:56:08 -0300

    Update FaixaDestaque.js

--- components/FaixaDestaque.js
+++ components/FaixaDestaque.js
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 
 export default function FaixaDestaque() {
   return (
-    <div className="w-full bg-black text-center py-6 px-4 text-sm text-neutral-400 border-t border-neutral-80">
+    <div className="w-full bg-black text-center py-6 px-4 text-sm text-neutral-400 border-t border-neutral-1000">
       Este teste foi desenvolvido seguindo padrões internacionais de triagem em saúde mental e respeita as normas éticas e de privacidade.
       É uma ferramenta informativa e não substitui diagnóstico médico ou psicológico.
     </div>
