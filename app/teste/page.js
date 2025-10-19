fbe44267f7bdcd9aec7305085937cf2e5e4d5386
Funcionamente <funcionamente.oficial@gmail.com>
2025-10-19 03:09:02 -0300

    Update page.js

--- app/teste/page.js
+++ app/teste/page.js
@@ -162,10 +162,10 @@ export default function TesteTDAH() {
               Ver Resultado
             </button>
 
-            /*<p className="mt-8 text-sm text-neutral-400">
+            {/*<p className="mt-8 text-sm text-neutral-400">
               Sua pontuação total: {pontuacaoTotal} pontos
             </p>
-          </motion.div>*/
+          </motion.div>*/}
         )}
       </div>
 
