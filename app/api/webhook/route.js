b2160b24e47df5a19695b239e0d5d626d70b30d4
Funcionamente <funcionamente.oficial@gmail.com>
2025-10-31 17:43:19 -0300

    Update route.js

--- app/api/webhook/route.js
+++ app/api/webhook/route.js
@@ -1,4 +1,4 @@
-i​mport { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 const SUPABASE_URL = process.env.SUPABASE_URL;
 const SUPABASE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
