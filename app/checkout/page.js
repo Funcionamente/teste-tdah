ca0457821361e8d52d6cddcd3eb7d3ec9bd360bd
Funcionamente <funcionamente.oficial@gmail.com>
2025-11-18 12:36:45 -0300

    Update page.js

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -1,6 +1,6 @@
 "use client";
 import { motion } from "framer-motion";
-import { useState, useEffect } from "react";​
+import { useState, useEffect } from "react";
 
 export default function CheckoutPage() {
   const [loading, setLoading] = useState(false);
