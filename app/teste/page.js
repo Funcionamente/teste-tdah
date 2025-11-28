79e14d64044ff15660437c081935c9f6b06203e5
Funcionamente <funcionamente.oficial@gmail.com>
2025-11-28 15:18:58 -0300

    Update page.js

--- app/teste/page.js
+++ app/teste/page.js
@@ -83,7 +83,7 @@ export default function TesteTDAH() {
           pontuacao: Number(pontuacaoTotal) || 0,
           interpretacao: null,
           id_pagamento: refPagamento,
-          status_pagamento: "pendente",
+          status_pagamento: "pending",
           resultado_exibido: false,
         },
       ]);
